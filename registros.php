c773af414a27139fc4da4a4ce0af19ed44177a1b
KevinBatres <kevinbatres@kbdcorporation.com>
2018-10-24 11:39:36 -0700

    Update registros.php

---
+++ registros.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Registro</title>
+
+	<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+	
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+	
+	<link rel="stylesheet" type="text/css" href="assets/css/sregistros.css">
+
+	<!-- Our Custom CSS -->
+<link rel="stylesheet" href="assets/css/aside.css">
+<!-- Scrollbar Custom CSS -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/malihu-custom-scrollbar-plugin/3.1.5/jquery.mCustomScrollbar.min.css">
+
+
+	
+</head>
+<body id="bgeneral">
+	<header style="width: 100%; height: 100px; background-color: #1c1cc4; margin-bottom: 20px;">
+		<button onclick="location='nregistro.html'" style="background: none; border: none;"><img style="width: 40px; height: 40px; position: absolute; right: 20px; top: 25px;" class=" img-fluid" src="assets/img/agregar.png" alt="card image"></button>
+	</header>
+	
+		
+	<section class="">
+		<div class="container">
+	<table bgcolor="#fffff" class="table" id="table1">
+		<thead class="thead-dark">
+			<tr>
+				<th>
+					<h4>ID</h4>
+				</th>
+				<th>
+					<h4>nombre</h4>
+				</th>
+				<th>
+					<h4>tipo</h4>
+				</th>
+				<th>
+					<h4>opciones</h4>
+				</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<td>
+					1
+				</td>
+				<td>
+					Paciente 1
+				</td>
+				<td>
+					interno
+				</td>
+				<td>
+					
+				</td>
+			</tr>
+			<tr>
+				<td>
+					2
+				</td>
+				<td>
+					Paciente 2
+				</td>
+				<td>
+					externo
+				</td>
+				<td>
+					
+				</td>
+			</tr>
+		</tbody>
+	</table>
+	</div>
+	</section>
+
+
+
+
+  <!-- jQuery CDN - Slim version (=without AJAX) -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <!-- Popper.JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+    <!-- Bootstrap JS -->
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+    <!-- jQuery Custom Scroller CDN -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/malihu-custom-scrollbar-plugin/3.1.5/jquery.mCustomScrollbar.concat.min.js"></script>
+</body>
+</html>
