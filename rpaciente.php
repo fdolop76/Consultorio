9808297eea83f27ee9890b394630fbe2ca6b8720
KevinBatres <kevinbatres@kbdcorporation.com>
2018-11-06 17:31:55 -0700

    Update rpaciente.php

---
+++ rpaciente.php
@@ -0,0 +1,42 @@
+<?php 
+require 'conexion.php';
+if (!$mysqli) {
+	echo 'Error al intentar conectar con la base de datos';
+} else {
+	echo $nombre = $_POST['nombre'];
+	echo $apellido = $_POST['apellido'];
+	echo $matricula = $_POST['matricula'];
+	echo $edad = $_POST['edad'];
+	echo $genero = $_POST['genero'];
+	echo $procedencia = $_POST['proviene'];
+	echo $domicilio = $_POST['domicilio'];
+	echo $telefono = $_POST['nfono'];
+	echo $religion = $_POST['religion'];
+	echo $educacion = $_POST['fnac'];
+	echo $correo = $_POST['educacion'];
+	echo $tipo = $_POST['tipo'];
+	echo $ecivil = $_POST['ecivil'];
+	echo $tutor = $_POST['tutor'];
+	
+
+$qVerificar = "SELECT * from paciente where nombre = '$nombre' and apellido = '$apellido'";
+		$verificar = $mysqli->query($qVerificar);
+		$ver=$verificar->num_rows;
+		echo $ver;
+		if($ver==0){
+
+			echo $query = "INSERT into paciente (id_paciente, matricula, nombre, apellido, edad, genero, proviene, domicilio, nfono, religion, educacion, correo, tipo, ecivil, tutor) 
+				values ('null', '$matricula', '$nombre', '$apellido', '$edad', '$genero', '$procedencia', '$domicilio', '$telefono', '$religion', '$educacion', '$correo', '$tipo', '$ecivil', '$tutor')";
+
+			$insertar = $mysqli->query($query);
+
+				if ($mysqli->affected_rows) {
+				header('location: registros.php');
+			} else { header('location: error.php'); 
+		}
+
+	} else {
+			header('location: correoExistente.php');
+	}
+}
+ ?>
